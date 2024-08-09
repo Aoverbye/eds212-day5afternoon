026f40a286df067735e78f014e2a990551e3230d
Aoverbye <amandaoverbye@gmail.com>
2024-08-09 16:41:44 -0500

    true false statement basics

---
+++ intro-logicals1.R
@@ -0,0 +1,37 @@
+2 < 5
+2 == 5
+4>7
+"A" < "D"
+"Z" < "Q"
+"cat" < "dog"
+"yeti" < "armadillo"
+ #create two vectors
+dogs <- c(1, 10, 4, 5, 18)
+cats <- c(0, 12, 10, 5, 2)
+dogs < cats
+
+dog_names <- c("Teddy", "khora", "banjo", "tallie")
+cat_names <- c("henrietta", "panda", "merlin", "runt")
+dog_names > cat_names
+
+2 <= 5
+6 <= 5
+
+burritos <- c(-4, 10, 1, -5, 9)
+tacos <- c(5, 3, 8, -5, 6)
+
+tacos <= burritos
+
+#exact matches ----
+
+5 == 5
+
+5 == TRUE
+5 == FALSE
+1 == TRUE
+0 == FALSE
+
+
+5  != 6
+
+
